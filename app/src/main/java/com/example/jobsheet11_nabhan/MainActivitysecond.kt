801c589529b3f7e02d46bace6fcbfac25f0a36f1
jobsheet22 <JMIMIMMUIMI@gmail.com>
2022-11-08 13:57:56 +0700

    first commit

---
+++ app/src/main/java/com/example/jobsheet11_nabhan/MainActivitysecond.kt
@@ -0,0 +1,11 @@
+package com.example.jobsheet11_nabhan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class second_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activitysecond)
+    }
+}
---
+++ app/src/main/java/com/example/jobsheet11_nabhan/SecondActivity.java
@@ -0,0 +1,6 @@
+package com.example.jobsheet11_nabhan;
+
+import android.app.Activity;
+
+public class SecondActivity extends Activity {
+}
